fa49a1a2c10f21173e8b7570a9de1eee7e226ee4
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-11-13 01:07:59 +0500

    new

--- scripts/sync_s3.sh
+++ scripts/sync_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Starting post-build phase: syncing files to S3..."
-aws s3 sync /home/ubuntu/VA/.next s3://codepipeline-us-west-1-209605742217/VA-CAL-PIPELINe --delete
-echo "Files successfully synced to S3."
+cd /home/ubuntu/VA
+npm install  # Install dependencies
+npm run build  # Build the Next.js application
