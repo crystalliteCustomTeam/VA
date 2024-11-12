9b66437ad7bec8712d9e25268daa1eb35298e066
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-11-13 02:09:17 +0500

    nhw

--- scripts/git-pull.sh
+++ scripts/git-pull.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
-cd /home/ubuntu/VA
+# #!/bin/bash
+# cd /home/ubuntu/VA
 
-# Reset any changes that are not committed
-git reset --hard HEAD
+# # Reset any changes that are not committed
+# git reset --hard HEAD
 
-# Clean any untracked files (optional)
-git clean -fd
+# # Clean any untracked files (optional)
+# git clean -fd
 
-# Optionally, remove specific conflicting files (like banner.module.scss)
-sudo rm -f /home/ubuntu/VA/styles/about/banner.module.scss
-sudo rm -f /home/ubuntu/VA/styles/about/bannerclients.module.scss
+# # Optionally, remove specific conflicting files (like banner.module.scss)
+# sudo rm -f /home/ubuntu/VA/styles/about/banner.module.scss
+# sudorm -f /home/ubuntu/VA/styles/about/bannerclients.module.scss
 
 
-git pull 
+# git pull 
+
+echo "helo"
