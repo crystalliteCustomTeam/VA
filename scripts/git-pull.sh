1732f31194e3668286285a115f71b05e99f9f8ca
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-11-13 01:54:12 +0500

    helo

--- scripts/git-pull.sh
+++ scripts/git-pull.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/VA || exit
+
+cd /home/ubuntu/VA || exit
+if [ -f /home/ubuntu/VA/styles/about/banner.module.scss ]; then
+  echo "File exists, deleting: banner.module.scss"
+  rm -f /home/ubuntu/VA/styles/about/banner.module.scss
+fi
+
 git pull 
