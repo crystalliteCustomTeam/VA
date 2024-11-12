878f2961d66acf74a1a7d3dbd767bf083182f5ac
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-11-13 01:29:14 +0500

    hewllo

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/VA  # Navigate to your app's directory
-git fetch --all  # Fetch the latest changes
-git reset --hard origin/main  # Reset any local changes to match the remote
+rm -f /home/ubuntu/VA/styles/about/bannerclients.module.scss
+
 git pull origin main  # Pull the latest changes from the main branch
