dd057f3dee2bab12023067d7fbfbc549b065dcbc
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-11-13 02:10:54 +0500

    hewlo

--- scripts/install-and-build.sh
+++ scripts/install-and-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/VA || exit
-npm install # Install dependencies
-npm run build # Build the Next.js app
+# npm install # Install dependencies
+# npm run build # Build the Next.js app
