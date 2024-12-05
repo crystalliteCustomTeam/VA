5e219e7f5a6199d8a16448682869a3eec6cba71e
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2024-12-05 22:47:20 +0500

    add live chat

--- src/app/Scripts.jsx
+++ src/app/Scripts.jsx
@@ -1,53 +1,55 @@
 "use client"
-import Script from 'next/script';
-import { useEffect } from 'react'
+import Script from "next/script"
+import { useEffect } from "react"
 
 const Scripts = () => {
-    useEffect(() => {
-        //======== Live Chat ========//
-        window.__lc = window.__lc || {};
-        window.__lc.license = 18818250;
-        window.__lc.integration_name = "manual_onboarding";
-        window.__lc.product_name = "livechat";
-
-        const livechat = document.createElement("script");
-        livechat.async = true;
-        livechat.src = "https://cdn.livechatinc.com/tracking.js";
-        document.body.appendChild(livechat);
-        //======== Google Tag Manager ========//
-        const gtmConfigScript1 = document.createElement("script");
-        gtmConfigScript1.id = "google-tag-manager-one";
-        gtmConfigScript1.src = "https://www.googletagmanager.com/gtag/js?id=AW-10860906782";
-        gtmConfigScript1.text = `
+  useEffect(() => {
+    // Load Tawk.to chat script
+    const timer = setTimeout(() => {
+      const tawkScript = document.createElement("script")
+      tawkScript.async = true
+      tawkScript.src =
+        "https://embed.tawk.to/66d1ff6150c10f7a00a22440/1i6i4r5nd"
+      tawkScript.charset = "UTF-8"
+      tawkScript.setAttribute("crossorigin", "*")
+      document.body.appendChild(tawkScript)
+    }, 5000)
+    //======== Google Tag Manager ========//
+    const gtmConfigScript1 = document.createElement("script")
+    gtmConfigScript1.id = "google-tag-manager-one"
+    gtmConfigScript1.src =
+      "https://www.googletagmanager.com/gtag/js?id=AW-10860906782"
+    gtmConfigScript1.text = `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-FJ46LGNLW0');
-        `;
-        document.head.appendChild(gtmConfigScript1);
-        //======== Google Tag Manager ========//
-        const gtmConfigScript2 = document.createElement("script");
-        gtmConfigScript2.id = "google-tag-manager-two";
-        gtmConfigScript2.src = "https://www.googletagmanager.com/gtag/js?id=G-FJ46LGNLW0";
-        gtmConfigScript2.text = `
+        `
+    document.head.appendChild(gtmConfigScript1)
+    //======== Google Tag Manager ========//
+    const gtmConfigScript2 = document.createElement("script")
+    gtmConfigScript2.id = "google-tag-manager-two"
+    gtmConfigScript2.src =
+      "https://www.googletagmanager.com/gtag/js?id=G-FJ46LGNLW0"
+    gtmConfigScript2.text = `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-FJ46LGNLW0');
-        `;
-        document.head.appendChild(gtmConfigScript2);
-    }, []);
+        `
+    document.head.appendChild(gtmConfigScript2)
+  }, [])
 
-    return (
-        <>
-            <Script id="one">
-                {`gtag('event', 'conversion', {'send_to': 'AW-10860906782/B05OCOOZ2aUDEJ6S8boo'});`}
-            </Script>
-            <Script id="two">
-                {`gtag('event', 'conversion', {'send_to': 'AW-10860906782/-cBLCMe7q54ZEJ6S8boo'});`}
-            </Script>
-        </>
-    );
+  return (
+    <>
+      <Script id="one">
+        {`gtag('event', 'conversion', {'send_to': 'AW-10860906782/B05OCOOZ2aUDEJ6S8boo'});`}
+      </Script>
+      <Script id="two">
+        {`gtag('event', 'conversion', {'send_to': 'AW-10860906782/-cBLCMe7q54ZEJ6S8boo'});`}
+      </Script>
+    </>
+  )
 }
 
 export default Scripts
