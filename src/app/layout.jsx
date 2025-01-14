ba805e043c3cb04428972c12b6021fdddaec8ca0
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2025-01-14 22:37:16 +0500

    locations updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,34 +1,46 @@
-
-import { Poppins } from "next/font/google";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "@/src/app/globals.scss";
-import Header from "@/src/app/layout/header";
-import Footer from "@/src/app/layout/footer";
-import Scripts from "./Scripts";
-
+import { Poppins } from "next/font/google"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@/src/app/globals.scss"
+import Header from "@/src/app/layout/header"
+import Footer from "@/src/app/layout/footer"
+import Scripts from "./Scripts"
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
-  display: 'swap',
-});
-
+  display: "swap",
+})
 
 export const metadata = {
-  title: "Professional Video Animation Company",
-  description: "Transform your ideas into stunning animations with our video animation company. We deliver high-quality, engaging content tailored to your needs.",
+  metadataBase: new URL("https://videoanimation.us"),
+  title: "Video Animation Studio - Official Site",
+  description:
+    "The official site of Animation Studios. Transform your ideas into stunning animations with our video animation company. We responsible for creating some of the most beloved films and animations ever made.",
   //===== Google Varification =====
   verification: {
     other: {
-      "google-site-verification": ['btqt1QyuPBr3EkUgOcgoKk46tz6ZrUfJ53kiCjQj7fA'],
+      "google-site-verification": [
+        "btqt1QyuPBr3EkUgOcgoKk46tz6ZrUfJ53kiCjQj7fA",
+      ],
     },
+  },
+  // Canonical
+  alternates: {
+    canonical: "https://videoanimation.us/",
+  },
+  other: {
+    "DC.title": "Video Animation Studio - Official Site",
+    "geo.region": ["US-FL", "US-TX"],
+    "geo.placename": ["Orlando", "Austin"],
+    "geo.position": ["28.542111;-81.37903", "30.271129;-97.743699"],
+    ICBM: ["28.542111, -81.37903", "30.271129, -97.743699"],
   },
   //===== No-Index =====
   robots: {
     index: false,
     follow: false,
   },
-};
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -40,5 +52,5 @@ export default function RootLayout({ children }) {
         <Footer />
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,24 @@
-import { BannerData, list, slides, ClientsReview, WhyItems, WhyOption, ClientsLogos } from "@/src/app/home/data/data";
+import {
+  BannerData,
+  list,
+  slides,
+  ClientsReview,
+  WhyItems,
+  WhyOption,
+  ClientsLogos,
+} from "@/src/app/home/data/data"
 
-import Banner from "@/src/app/home/banner";
-import KeyPoints from "@/src/app/home/keypoints";
-import Services from "@/src/app/home/services";
-import Portfolio from "@/src/app/home/portfolio";
-import Testimonials from "@/src/app/home/testimonials";
-import WhyChooes from "@/src/app/home/whychoose";
-import VideoCta from "@/src/app/home/videocta";
-import Clients from "@/src/app/home/clients";
-import SpotLight from "@/src/app/home/spotlight";
-import Faqs from "@/src/app/home/faq";
-import Industries from "@/src/app/home/industries";
+import Banner from "@/src/app/home/banner"
+import KeyPoints from "@/src/app/home/keypoints"
+import Services from "@/src/app/home/services"
+import Portfolio from "@/src/app/home/portfolio"
+import Testimonials from "@/src/app/home/testimonials"
+import WhyChooes from "@/src/app/home/whychoose"
+import VideoCta from "@/src/app/home/videocta"
+import Clients from "@/src/app/home/clients"
+import SpotLight from "@/src/app/home/spotlight"
+import Faqs from "@/src/app/home/faq"
+import Industries from "@/src/app/home/industries"
 
 export default function Home() {
   return (
@@ -26,6 +34,48 @@ export default function Home() {
       <Clients ClientsLogos={ClientsLogos} />
       <Industries slides={slides} />
       <Faqs data={list} />
+
+      {/* Website Schema */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org/",
+            "@type": "WebSite",
+            name: "Video Animation Studio - Official Site",
+            url: "https://videoanimation.us/",
+            potentialAction: {
+              "@type": "SearchAction",
+              target: "{search_term_string}",
+              "query-input": "required name=search_term_string",
+            },
+          }),
+        }}
+      />
+
+      {/* Corporation Schema */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org/",
+            "@type": "Corporation",
+            name: "Video Animation Studio - Official Site",
+            alternateName: "Video Animation Company",
+            url: "https://videoanimation.us/",
+            logo: "",
+            sameAs: [
+              "https://www.facebook.com/videoanimationus",
+              "https://www.x.com/vid_animationus",
+              "https://www.instagram.com/videoanimationus",
+              "https://www.linkedin.com/company/video-animation-us",
+              "https://www.youtube.com/@VideoAnimationStudio",
+              "https://videoanimation.us/",
+              "https://www.pinterest.com/animationvideous/",
+            ],
+          }),
+        }}
+      />
     </>
-  );
+  )
 }
